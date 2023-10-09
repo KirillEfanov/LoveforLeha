afcb6a731c4d6942acda90639c29b897e02daeb9
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2023-10-09 15:58:59 +0500

    Update main.lua

--- love/main.lua
+++ love/main.lua
@@ -1,29 +1,44 @@
+function Rectangle(x, y, w, h)
+    local this = {}
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h= h
+
+    function this:draw()
+        love.graphics.rectangle("fill", self.x, self.y, self.w, self.h) -- по x вправо
+    end
+
+    return this
+end
+
+
 function love.load()
     font = love.graphics.newFont(18)
     love.window.setMode(1280, 720)
-    
-    mouseX = 0 
-    mouseY = 0
-
-    xrectangle = 20
-    yrectangle = 640
-
-    Xbutton1 = 1100
-    Ybutton1 = 50
-    Widthbutton1 = 50
-    Heightbutton1 = 50
-
-    Xbutton2 = 1040
-    Ybutton2 = 50
-    Widthbutton2 = 50
-    Heightbutton2 = 50
-
-    Heightrectangle = -100
+    rectangle = Rectangle(20, 640, 100,50)
+    button1 = Rectangle(1100, 50, 50, 50)
+    button2 = Rectangle(1040, 50, 50, 50)
 end
 
-function love.mousemoved(x,y)
-    mouseX = x 
-    mouseY = y
+function love.update()
+    local x, y = love.mouse.getPosition()
+    if love.mouse.isDown("1") then -- перемещение по X вправо
+        if x >= button1.x and x < button1.x + button1.w and y > button1.y and y < button1.y + button1.h then
+            if rectangle.x < 1060 then
+                rectangle.x = rectangle.x + 10
+            end
+        end
+    end
+
+    if love.mouse.isDown("1") then -- перемещение по X влево
+        if x >= button2.x and x < button2.x + button2.w and y > button2.y and y < button2.y + button2.h then
+            if rectangle.x >= 30 then
+                rectangle.x = rectangle.x - 10
+            end
+        end
+    end
+
 end
 
 function love.draw()
@@ -33,27 +48,7 @@ function love.draw()
 	love.graphics.line(10,28, 10,700, 1200,700) --оси координат
     love.graphics.line(15,38, 10,28, 5,38) --стрелка по X
     love.graphics.line(1190,695, 1200,700, 1190,705) --стрелка по Y
-    if love.mouse.isDown("1") then -- перемещение по X вправо
-        if mouseX >= Xbutton1 and mouseX < Xbutton1 + Widthbutton1 and mouseY > Ybutton1 and mouseY < Ybutton1 + Heightbutton1 then
-            if xrectangle < 1060 then
-                xrectangle = xrectangle + 10
-            end
-        end
-    end
-
-    if love.mouse.isDown("1") then -- перемещение по X влево
-        if mouseX >= Xbutton2 and mouseX < Xbutton2 + Widthbutton2 and mouseY > Ybutton2 and mouseY < Ybutton2 + Heightbutton2 then
-            if xrectangle >= 30 then
-                xrectangle = xrectangle - 10
-            end
-        end
-    end
-
-    love.graphics.print("Lesha loh", 50, 30)
-
-    love.graphics.rectangle("fill", Xbutton1, Ybutton1, Widthbutton1, Heightbutton1) -- по x вправо
-    love.graphics.rectangle("fill", Xbutton2, Ybutton2, Widthbutton2, Heightbutton2) -- по x влево
-    love.graphics.rectangle("fill", xrectangle, yrectangle, 100, 50) --робот 1 блок
-    love.graphics.rectangle("fill", xrectangle + 40, yrectangle, 20, Heightrectangle) --робот 2 блок
+    rectangle:draw()
+    button1:draw()
+    button2:draw()
 end
-
